90b39de8657dbbd9a4a71132dee22c36691086a9
Dev Palacio <danielpalaciouribe@gmail.com>
2025-12-17 08:20:08 -0500

    feat: agregar servicio para consumir usuarios desde API pública

---
+++ src/app/core/services/users.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { User } from '../../shared/models';
+
+interface RandomUserResponse {
+  results: Array<{
+    login: { uuid: string };
+    name: { first: string; last: string };
+    email: string;
+  }>;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  private readonly apiUrl = 'https://randomuser.me/api/';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(limit = 10): Observable<User[]> {
+    return this.http
+      .get<RandomUserResponse>(`${this.apiUrl}?results=${limit}`)
+      .pipe(
+        map((response) =>
+          response.results.map((user) => ({
+            id: user.login.uuid,
+            name: `${user.name.first} ${user.name.last}`,
+            email: user.email,
+          }))
+        )
+      );
+  }
+}
